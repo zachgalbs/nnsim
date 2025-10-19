33a5e65330fe1b42de723df3284bb7ace6550de1
yard <106509725+a-hiremath@users.noreply.github.com>
2025-10-18 19:51:56 -0700

    implemented window initialization
    hella advanced window initialization software

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "raylib.h"
 using namespace std;
 
 int main() {
-	cout << "Hello!";
+	const int screenW = 800, screenH = 450;
+	InitWindow(screenW, screenH, "fuck you");
+
+	
+
+	_sleep(1000);
+
+	CloseWindow();
 
 	return 0;
 }
