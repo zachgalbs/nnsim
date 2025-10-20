052d7b5d93f38a63f0c3a8c19a714d80050953a4
yard <106509725+a-hiremath@users.noreply.github.com>
2025-10-19 18:02:40 -0700

    initialized window, created player class, added basic movement

---
+++ nnsim_/constants.h
@@ -0,0 +1,5 @@
+//
+// Created by adity on 10/19/2025.
+//
+
+const int screenW = 800, screenH = 450;
--- nnsim_/main.cpp
+++ nnsim_/main.cpp
@@ -1,16 +1,82 @@
+#include <iostream>
 #include <stdlib.h>
 #include <thread>
 #include "raylib.h"
+#include "constants.h"
+#include <cmath>
 
 using namespace std;
 
+
+class Car {
+public:
+    double vel;
+    double mass = 5;
+    double xPos;
+    double yPos;
+    double vSize = 10;
+    Color vColor = RED;
+    double theta;
+    double steeringAngle;
+
+    // * methods
+
+    Car(double xPos, double yPos) : xPos(xPos), yPos(yPos) {}
+
+    void updatePos(double passedTime) {
+        xPos += vel * passedTime * cos(theta);
+        yPos += vel * passedTime * sin(theta);
+
+        theta += steeringAngle * passedTime;
+
+        if (vel > 0) {
+            vel -= 10 * passedTime;
+        }
+
+    }
+
+
+
+};
+
+
 int main() {
 
-    InitWindow(800, 450, "window");
+    InitWindow(screenW, screenH, "window");
+    SetTargetFPS(120);
+
+    Car player(screenW / 2, screenH / 2);
+
+    std::cout << player.xPos << " " << player.yPos << std::endl << player.vSize;
+
+    while(!WindowShouldClose()) {
+
+        ClearBackground(BLACK);
+
+        if (IsKeyDown(KEY_W)) {
+            player.vel++;
+        }
+
+        if (IsKeyDown(KEY_A) && player.steeringAngle < 5) {
+            player.steeringAngle ++;
+        }
+
+        if (IsKeyDown(KEY_D) && player.steeringAngle > -5) {
+            player.steeringAngle --;
+        }
+
+        player.updatePos(GetFrameTime());
+
+        BeginDrawing();
+        DrawCircle(player.xPos, player.yPos, player.vSize, player.vColor);
+        EndDrawing();
+
+
+    }
 
 
 
-    CloseWindow();
+
 
     return 0;
 }
